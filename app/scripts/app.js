d5ea899a4eba9c49260a06ce6cf8efc5a0a47e52
AbelAlejandro <abel.alejandro@ymail.com>
2015-06-11 14:25:16 -0300

    App created

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name expressTestApp
+ * @description
+ * # expressTestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('expressTestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
